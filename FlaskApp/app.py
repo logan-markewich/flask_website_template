39f681b17757f44492caea4bf949862fd4f78cbf
logan-markewich <logan.markewich@live.com>
2017-09-26 13:00:54 -0600

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, json, request
-from flask.ext.mysql import MySQL
+from flask_mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
