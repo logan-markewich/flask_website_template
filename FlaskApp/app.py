640ed6343e8b4f87ba8a0c1e495c0479f8c56d1e
logan-markewich <logan.markewich@live.com>
2017-09-26 10:59:17 -0600

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask import json
+from flask import request
 
 app = Flask(__name__)
 
@@ -11,6 +13,20 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+@app.route('/signUp',methods=['POST'])
+def signUp():
+    #read in values from ui
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+    
+    #validate values from ui
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    
 if __name__ == "__main__":
     app.run()
     
