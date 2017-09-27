0147eebd6ad262d996f48c521304ae7d95afbb0f
logan-markewich <logan.markewich@live.com>
2017-09-27 10:54:07 -0600

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -146,12 +146,13 @@ def getWish():
             
             wishes_dict = []
             for wish in wishes:
-                wish_dict = {
-                    'Id': wish[0],
-                    'Title': wish[1],
-                    'Description': wish[2],
-                    'Date': wish[4]}
-            wishes_dict.append(wish_dict)
+                if wish[1] != "Wish Title":
+                    Wish_dict = {
+                        'Id': wish[0],
+                        'Title': wish[1],
+                        'Description': wish[2],
+                        'Date': wish[4]}
+                        wishes_dict.append(wish_dict)
             
             return json.dumps(wishes_dict)
         else:
