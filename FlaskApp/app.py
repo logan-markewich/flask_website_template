c62fe0d4912cf811664453fba25641fcde8e82da
logan-markewich <logan.markewich@live.com>
2017-09-27 10:08:12 -0600

    Update app.py

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -132,6 +132,32 @@ def addWish():
     finally:
         cursor.close()
         conn.close()
+        
+@app.route('/getWish')
+def getWish():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+            
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetWishByUser',(_user,))
+            wishes = cursor.fetchall()
+            
+            wishes_dict = []
+            for wish in wishes:
+                wish_dict = {
+                    'Id': wish[0],
+                    'Title': wish[1],
+                    'Description': wish[2],
+                    'Date': wish[4]}
+            wishes_dict.append(wish_dict)
+            
+            return json.dumps(wishes_dict)
+        else:
+            return render_template('error.html', error = "Unauthorized access")
+    except Exception as e:
+        return render_template('error.html',error = str(e))
     
 if __name__ == "__main__":
     app.run()
