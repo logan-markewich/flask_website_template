c47fbd0d16aaf2ffd080baf4e6c464b40ff5085f
logan-markewich <logan.markewich@live.com>
2017-09-29 16:19:30 -0600

    forget to add part3 files

---
+++ FlaskApp/config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True;
+SECRET_KEY = 'my-dog-is-kinda-a-big-deal'
+
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'https://openid.aol.com.<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
