5b357fcc0d6e90eed8b5897f0e7bcfccfc6aaa64
logan-markewich <logan.markewich@live.com>
2017-10-16 10:45:02 -0600

    Finished part 4 of tut

--- FlaskApp/config.py
+++ FlaskApp/config.py
@@ -1,5 +1,7 @@
+import os
 WTF_CSRF_ENABLED = True;
 SECRET_KEY = 'my-dog-is-kinda-a-big-deal'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 OPENID_PROVIDERS = [
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
@@ -7,3 +9,6 @@ OPENID_PROVIDERS = [
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
